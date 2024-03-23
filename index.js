f3a9ac13c6795ecd1ea4bee0d8b9062f72969455
GithinjiKelvin <githinjikelvin59@gmail.com>
2024-03-23 20:39:13 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+var customerName = 'bob'
+var bestCustomer
+const favCustomer = 'Kev'
+
+function upperCaseCustomerName(custName){
+    // console.log(customerName)
+    customerName=customerName.toUpperCase()
+    // console.log(customerName)
+    // return custName.toUpperCase()
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+    return bestCustomer
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer(){
+    favCustomer = 'bob'
+    if(favCustomer === bob){
+        console.log(`Your favorite customer is ${favCustomer}`)
+    }else{
+        console.log('Assignment to constant variable.')
+    }
+}
+
